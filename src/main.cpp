3d181c7217e9399ebfa93edab177dfae859219cc
James Clark <james@citcs.org>
2024-07-12 05:21:47 +0100

    voltage sweep test and parser is working :D

--- src/main.cpp
+++ src/main.cpp
@@ -93,6 +93,11 @@ void doprompt( uint8_t requestedstate ) {
 }
 
 
+/// @brief Do a tab.
+void dotab() {
+  Serial.print("\t");
+}
+
 
 
 
@@ -109,10 +114,37 @@ void command_zifalldis();
 void command_zifreset();
 
 
-
+/// @brief Perform a self test with the 0-15 board.
 void command_selftest_a();
+/// @brief Perform a self test with the 0-1 board.
+void command_selftest_b();
 
+/// @brief Perform a functionality check of a gate.
+/// @param inputpin The input pin to check.
+/// @param outputpin The output pin to check.
+/// @return The results of the test.
+uint8_t command_test_gate1to1(  uint8_t inputpin , uint8_t outputpin );
 
+/// @brief Perform a functionality check of the gate.
+/// @param inputpina The first input pin to check.
+/// @param inputpinb The second input pin to check.
+/// @param outputpin The output pin to record.
+/// @return The results of the test.
+uint8_t command_test_gate2to1(  uint8_t inputpina , uint8_t inputpinb , uint8_t outputpin );
+
+/// @brief Perform a voltage sweep test and output CSV values over the serial port.
+/// @param inputpin The input pin to check.
+/// @param outputpin The output pin to check.
+void command_test_vsweepu( uint8_t inputpin , uint8_t outputpin , uint16_t stepsize = 128 );
+
+void command_test_vsweepu_parser( String & commandline );
+
+/// @brief Perform a voltage sweep test and output CSV values over the serial port.
+/// @param inputpin The input pin to check.
+/// @param outputpin The output pin to check.
+void command_test_vsweepd( uint8_t inputpin , uint8_t outputpin , uint16_t stepsize = 128 );
+
+void command_test_vsweepd_parser( String & commandline );
 
 
 
@@ -155,6 +187,8 @@ void loop() {
 
   if ( commandline.startsWith( "selftesta" ) ) { command_selftest_a(); }
 
+  if ( commandline.startsWith( "vsweepu" ) ) { command_test_vsweepu_parser( commandline ); }
+  if ( commandline.startsWith( "vsweepd" ) ) { command_test_vsweepd_parser( commandline ); }
 
 }
 
@@ -292,7 +326,7 @@ void command_zifallena() {
 
 
 void command_zifalldis() {
-  Serial.print("All zif pins disabled.");
+  Serial.println("All zif pins disabled.");
   myzifbusobj.disableall();
 }
 
@@ -353,5 +387,243 @@ void command_selftest_a() {
 
 
 
+
+
+/// @brief Perform a self test with the 0-15 board.
+//void command_selftest_a();
+/// @brief Perform a self test with the 0-1 board.
+//void command_selftest_b();
+
+/// @brief Perform a functionality check of a gate.
+/// @param inputpin The input pin to check.
+/// @param outputpin The output pin to check.
+/// @return The results of the test.
+uint8_t command_test_gate1to1(  uint8_t inputpin , uint8_t outputpin ) {
+  return 0;
+}
+
+/// @brief Perform a functionality check of the gate.
+/// @param inputpina The first input pin to check.
+/// @param inputpinb The second input pin to check.
+/// @param outputpin The output pin to record.
+/// @return The results of the test.
+uint8_t command_test_gate2to1(  uint8_t inputpina , uint8_t inputpinb , uint8_t outputpin ) {
+  return 0;
+}
+
+
+
+
+
+
+void command_test_vsweepu_parser( String & commandline ) {
+
+  // remove command
+  commandline = commandline.substring( 8 );
+  commandline += " ";
+
+  // get the input pin
+  uint8_t inputpin = commandline.toInt();
+  commandline = commandline.substring( commandline.indexOf(" ") + 1 );
+
+  // get the output pin
+  uint8_t outputpin = commandline.toInt();
+  commandline = commandline.substring( commandline.indexOf(" ") + 1 );
+
+  // get the stepsize if any
+  uint16_t stepsize = 0;
+  if ( commandline.length() ) {
+    stepsize = commandline.toInt();
+  }
+
+  if ( stepsize == 0 ) { stepsize = 128; }
+
+  command_test_vsweepu( inputpin , outputpin , stepsize );
+
+};
+
+
+/// @brief Perform a voltage sweep test and output CSV values over the serial port.
+/// @param inputpin The input pin to check.
+/// @param outputpin The output pin to check.
+void command_test_vsweepu( uint8_t inputpin , uint8_t outputpin , uint16_t stepsize ) {
+
+  Serial.println("vsweep test");
+  Serial.print("Input: ");
+  Serial.println( inputpin );
+  Serial.print("Output: ");
+  Serial.println( outputpin );
+  Serial.print("Stepsize: " );
+  Serial.println( stepsize );
+
+  myzifbusobj.pin2bus( inputpin , ZIFBUS_VMVCC_0 );
+  myzifbusobj.enable( inputpin );
+
+  myzifbusobj.pin2bus( outputpin , ZIFBUS_LEDTOGND_0 );
+  myzifbusobj.enable( outputpin );
+
+  for ( uint16_t loopcounter = 0 ; loopcounter < 4096 ; loopcounter += stepsize ) {
+
+    Serial.print(loopcounter);
+    dotab();
+
+    // set the dac first
+    mysensorbusobj.switchto(VMVCC_0);
+    mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue( loopcounter );
+
+    // delay to let things settle
+    delay(5);
+
+    // input
+    mysensorbusobj.switchto(VMVCC_0);
+    Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getbusvoltage() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getcurrent() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getpower() ); dotab();
+
+    // output
+    mysensorbusobj.switchto(LEDTOGND_0);
+    Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getbusvoltage() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getcurrent() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getpower() );
+
+    Serial.println();
+    
+  }
+
+  Serial.print(4096); dotab();
+  mysensorbusobj.switchto(VMVCC_0);
+  mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue( 4095 );
+  delay(5);
+  mysensorbusobj.switchto(VMVCC_0);
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getbusvoltage() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getcurrent() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getpower() ); dotab();
+  mysensorbusobj.switchto(LEDTOGND_0);
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getbusvoltage() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getcurrent() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getpower() );
+  Serial.println();
+
+  // turn off the vmvcc!
+  mysensorbusobj.switchto(VMVCC_0);
+  mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue(0);
+
+  myzifbusobj.reset();
+
+}
+
+
+
+
+
+void command_test_vsweepd_parser( String & commandline ) {
+
+  // remove command
+  commandline = commandline.substring( 8 );
+  commandline += " ";
+
+  // get the input pin
+  uint8_t inputpin = commandline.toInt();
+  commandline = commandline.substring( commandline.indexOf(" ") + 1 );
+
+  // get the output pin
+  uint8_t outputpin = commandline.toInt();
+  commandline = commandline.substring( commandline.indexOf(" ") + 1 );
+
+  // get the stepsize if any
+  uint16_t stepsize = 0;
+  if ( commandline.length() ) {
+    stepsize = commandline.toInt();
+  }
+
+  if ( stepsize == 0 ) { stepsize = 128; }
+
+  command_test_vsweepd( inputpin , outputpin , stepsize );
+
+};
+
+/// @brief Perform a voltage sweep test and output CSV values over the serial port.
+/// @param inputpin The input pin to check.
+/// @param outputpin The output pin to check.
+void command_test_vsweepd( uint8_t inputpin , uint8_t outputpin , uint16_t stepsize ) {
+
+  Serial.println("vsweep test");
+  Serial.print("Input: ");
+  Serial.println( inputpin );
+  Serial.print("Output: ");
+  Serial.println( outputpin );
+  Serial.print("Stepsize: " );
+  Serial.println( stepsize );
+
+  myzifbusobj.pin2bus( inputpin , ZIFBUS_VMVCC_0 );
+  myzifbusobj.enable( inputpin );
+
+  myzifbusobj.pin2bus( outputpin , ZIFBUS_LEDTOGND_0 );
+  myzifbusobj.enable( outputpin );
+
+  Serial.print(4096); dotab();
+  mysensorbusobj.switchto(VMVCC_0);
+  mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue( 4095 );
+  delay(5);
+  mysensorbusobj.switchto(VMVCC_0);
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getbusvoltage() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getcurrent() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getpower() ); dotab();
+  mysensorbusobj.switchto(LEDTOGND_0);
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getbusvoltage() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getcurrent() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getpower() );
+  Serial.println();
+
+  for ( uint16_t loopcounter = 4096 - stepsize ; loopcounter > 0 ; loopcounter -= stepsize ) {
+
+    Serial.print(loopcounter);
+    dotab();
+
+    // set the dac first
+    mysensorbusobj.switchto(VMVCC_0);
+    mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue( loopcounter );
+
+    // delay to let things settle
+    delay(5);
+
+    // input
+    mysensorbusobj.switchto(VMVCC_0);
+    Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getbusvoltage() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getcurrent() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getpower() ); dotab();
+
+    // output
+    mysensorbusobj.switchto(LEDTOGND_0);
+    Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getbusvoltage() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getcurrent() ); dotab();
+    Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getpower() );
+
+    Serial.println();
+    
+  }
+
+  Serial.print(0); dotab();
+  mysensorbusobj.switchto(VMVCC_0);
+  mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue( 0 );
+  delay(5);
+  mysensorbusobj.switchto(VMVCC_0);
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getbusvoltage() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getcurrent() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[VMVCC_0].getpower() ); dotab();
+  mysensorbusobj.switchto(LEDTOGND_0);
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getbusvoltage() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getcurrent() ); dotab();
+  Serial.print( mysensorbusobj.ourina219obj[LEDTOGND_0].getpower() );
+  Serial.println();
+
+
+  // turn off the vmvcc!
+  mysensorbusobj.switchto(VMVCC_0);
+  mysensorbusobj.ourmcp4725obj[VMVCC_DAC_0].setvalue(0);
+
+  myzifbusobj.reset();
+
+}
 
 
